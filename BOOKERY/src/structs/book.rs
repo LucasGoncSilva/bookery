3fa43ab0cb30fd9f1d2496b6ed9eed44b78465f7
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2024-06-10 23:53:32 -0300

    creates tests for structs::book

--- BOOKERY/src/structs/book.rs
+++ BOOKERY/src/structs/book.rs
@@ -65,3 +65,89 @@ impl Book {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::fmt::{Debug, Formatter, Result as FmtResult};
+
+    use time::{error::ComponentRange, Month};
+
+    impl PartialEq for Book {
+        fn eq(&self, other: &Self) -> bool {
+            self.id == other.id
+                && self.name.as_str() == other.name.as_str()
+                && self.author_uuid == other.author_uuid
+                && self.editor.as_str() == other.editor.as_str()
+                && self.release == other.release
+        }
+    }
+
+    impl Debug for Book {
+        fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+            f.debug_struct("Book")
+                .field("id", &self.id)
+                .field("name", &self.name.as_str())
+                .field("author_uuid", &self.author_uuid)
+                .field("editor", &self.editor.as_str())
+                .field("release", &self.release)
+                .finish()
+        }
+    }
+
+    const DEFAULT_NAME: &'static str = "Name";
+    const DEFAULT_EDITOR: &'static str = "Editor";
+    const DEFAULT_RELEASE: Result<Date, ComponentRange> =
+        Date::from_calendar_date(2000, Month::January, 1);
+
+    #[test]
+    fn test_create_book() {
+        let payload_book: PayloadBook = PayloadBook {
+            name: DEFAULT_NAME.to_string(),
+            author_uuid: Uuid::new_v4(),
+            editor: DEFAULT_EDITOR.to_string(),
+            release: DEFAULT_RELEASE.unwrap(),
+        };
+
+        let book: Book = Book::create(payload_book).unwrap();
+
+        assert_eq!(
+            book,
+            Book {
+                id: book.id,
+                name: BookName::try_from(DEFAULT_NAME.to_string()).unwrap(),
+                author_uuid: book.author_uuid,
+                editor: EditorName::try_from(DEFAULT_EDITOR.to_string()).unwrap(),
+                release: DEFAULT_RELEASE.unwrap(),
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_book() {
+        let payload_update_book: PayloadUpdateBook = PayloadUpdateBook {
+            id: Uuid::new_v4(),
+            name: DEFAULT_NAME.to_string(),
+            author_uuid: Uuid::new_v4(),
+            editor: DEFAULT_EDITOR.to_string(),
+            release: DEFAULT_RELEASE.unwrap(),
+        };
+
+        let book_uuid: Uuid = payload_update_book.id.clone();
+        let book_author_uuid: Uuid = payload_update_book.author_uuid.clone();
+
+        let book: Book = Book::parse(payload_update_book).unwrap();
+
+        assert_eq!(
+            book,
+            Book {
+                id: book_uuid,
+                name: BookName::try_from(DEFAULT_NAME.to_string()).unwrap(),
+                author_uuid: book_author_uuid,
+                editor: EditorName::try_from(DEFAULT_EDITOR.to_string()).unwrap(),
+                release: DEFAULT_RELEASE.unwrap(),
+            }
+        );
+    }
+}
