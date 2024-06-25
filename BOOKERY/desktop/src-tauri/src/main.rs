10e1717192d954a074b70cf7be97b13cdc48a0b9
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2024-06-24 23:16:19 -0300

    starts comms for desktop front and back

--- BOOKERY/desktop/src-tauri/src/main.rs
+++ BOOKERY/desktop/src-tauri/src/main.rs
@@ -1,15 +1,102 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use serde::Deserialize;
+
+// use shared::structs::{
+//     author::Author, book::BookWithAuthor, costumer::Costumer, rental::RentalWithCostumerAndBook,
+// };
+
+#[derive(Deserialize, Clone)]
+enum Module {
+    Author,
+    Book,
+    Costumer,
+    Rental,
+}
+
+#[derive(Deserialize)]
+enum ModuleAction {
+    Create,
+    Delete,
+    Update,
+    GetRaw,
+    Get,
+    Search,
+    SearchRaw,
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            handle_actual_endpoint,
+            create_table_head
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn dispatch_module_endpoint(module: Module) -> &'static str {
+    match module {
+        Module::Author => "author",
+        Module::Book => "book",
+        Module::Costumer => "costumer",
+        Module::Rental => "rental",
+    }
+}
+
+fn dispatch_action_endpoint(action: ModuleAction, token: String) -> String {
+    match action {
+        ModuleAction::Create => String::from("create"),
+        ModuleAction::Delete => String::from("delete"),
+        ModuleAction::Update => String::from("update"),
+        ModuleAction::GetRaw => String::from("get-raw"),
+        ModuleAction::Get => String::from("get"),
+        ModuleAction::Search => format!("search?token={token}"),
+        ModuleAction::SearchRaw => format!("search-raw?token={token}"),
+    }
+}
+
+#[tauri::command]
+fn handle_actual_endpoint(module: Module, action: ModuleAction) -> String {
+    let module: &str = dispatch_module_endpoint(module);
+    let action: String = dispatch_action_endpoint(action, "".to_string());
+
+    format!("http://localhost:3000/{module}/{action}")
+}
+
+#[tauri::command]
+fn create_table_head(module: Module) -> &'static str {
+    match module {
+        Module::Author => {
+            "<tr>
+                <th>Name</th>
+                <th>Born</th>
+            </tr>"
+        }
+        Module::Book => {
+            "<tr>
+                <th>Name</th>
+                <th>Author</th>
+                <th>Editor</th>
+                <th>Release</th>
+            </tr>"
+        }
+        Module::Costumer => {
+            "<tr>
+                <th>Name</th>
+                <th>Document</th>
+                <th>Born</th>
+            </tr>"
+        }
+        Module::Rental => {
+            "<tr>
+                <th>Costumer</th>
+                <th>Book</th>
+                <th>Borrowed at</th>
+                <th>Due Date</th>
+                <th>Returned at</th>
+            </tr>"
+        }
+    }
 }
--- BOOKERY/desktop/src/main.js
+++ BOOKERY/desktop/src/main.js
@@ -1,18 +1,52 @@
-const { invoke } = window.__TAURI__.tauri;
-
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  const { invoke } = window.__TAURI__.tauri;
+  const tableHead = document.getElementById("table-head");
+  const output = document.getElementById("output");
+
+  function hideForms() {
+    document.querySelectorAll("form").forEach((form) => {
+      form.style.display = "none";
+    });
+  }
+
+  async function getDataAPI(module) {
+    tableHead.innerHTML = "";
+    tableHead.innerHTML = await invoke("create_table_head", {
+      module: module,
+    });
+
+    output.innerHTML = await invoke("handle_actual_endpoint", {
+      module: module,
+      action: "Search",
+    });
+  }
+
+  hideForms();
+
+  // Navbar btns
+  document.querySelectorAll(".module-btn").forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      // Page title
+      document.querySelectorAll(".module-name").forEach(function (span) {
+        span.textContent = btn.dataset["module"];
+      });
+
+      // Show module form
+      hideForms();
+      document.getElementById(btn.dataset["form"]).style.display =
+        "inline-grid";
+    });
   });
+
+  document
+    .getElementById("author-form-submit")
+    .addEventListener("click", function () {
+      let name = document.getElementById("author-form-name").value;
+      let bornFrom = document.getElementById("author-form-born-from").value;
+      let bornUntil = document.getElementById("author-form-born-until").value;
+
+      getDataAPI(this.dataset["module"]);
+
+      output.textContent = name;
+    });
 });
